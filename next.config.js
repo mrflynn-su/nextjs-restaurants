00e6ff6e173f5d42dd4d23413b097141b85dd56b
mrflynn-su <mrflynn@syr.edu>
2022-12-21 20:36:59 -0500

    added new host to next config js file

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
           protocol: 'http',
           hostname: 'ist-363-restaurants.local',
         },
+        {
+          protocol: 'https',
+          hostname: 'restaurants.ist363.xyz',
+        }
       ],
     },
   }
